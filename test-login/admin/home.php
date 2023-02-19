c2d2847ea29d853ce1b55f87371d98bb2bba99b6
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2023-02-19 23:27:02 +0100

    test-login

---
+++ test-login/admin/home.php
@@ -0,0 +1,26 @@
+﻿<?php
+	// Initialiser la session
+	session_start();
+	// Vérifiez si l'utilisateur est connecté, sinon redirigez-le vers la page de connexion
+	if(!isset($_SESSION["username"])){
+		header("Location: login.php");
+		exit(); 
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="../style.css" />
+	</head>
+	<body>
+		<div class="sucess">
+		<h1>Bienvenue <?php echo $_SESSION['username']; ?>!</h1>
+		<p>C'est votre espace admin.</p>
+		<a href="add_user.php">Add user</a> | 
+		<a href="#">Update user</a> | 
+		<a href="#">Delete user</a> | 
+		<a href="../logout.php">Déconnexion</a>
+		</ul>
+		</div>
+	</body>
+</html>
