4974c0baf2d97c86b799eeebe89c9f42fad37c1e
Dev&+ <45708451+Eliftb186@users.noreply.github.com>
2023-02-28 11:22:09 +0100

    explication des recherches

--- test.php
+++ test.php
@@ -1,5 +1,12 @@
 <?php
-session_start();
-require('config.php');
+    session_start();
+    require('config.php');
+    $liste = array();
+    foreach(recuperer_les_videos() as $id => $video){
+        if ($video['titre'] == "Kung Fu Panda"){
+            $liste[] = $id;
+        }
+    }
+    var_dump($liste);
 
 ?>
