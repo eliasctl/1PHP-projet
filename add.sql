0d918a077ca6e75c81c3c546a59078acd3acdbab
Le_Vrai_WIWI <superwoup@gmail.com>
2023-03-15 23:42:07 +0100

    this is spartaaaa !!!

---
+++ add.sql
@@ -0,0 +1,93 @@
+INSERT INTO
+    `videos`(
+        `id`,
+        `cathegorie`,
+        `annee`,
+        `titre`,
+        `realisateur`,
+        `acteurs`,
+        `description`,
+        `image`,
+        `prix`,
+        `spoiler`,
+        `telechargement`
+    )
+VALUES
+    (
+        NULL,
+        'action',
+        '2023',
+        'Le Loup de Wall Street',
+        'Martin Scorsese',
+        'Leonardo DiCaprio, Jonah Hill, Margot Robbie',
+        'Jordan Belfort est un jeune trader ambitieux qui rêve de devenir riche. Il se lance dans le monde de la bourse et devient rapidement un trader hors pair. Mais il va vite se rendre compte que la bourse est un monde impitoyable et qu’il va devoir faire des sacrifices pour réussir.',
+        'https://image.tmdb.org/t/p/original/dQIQZbJXn1pflQw3nwvXLJX0dHa.jpg',
+        '53',
+        'GT9UfSqBz9o',
+        'magie'
+    ),
+    (
+        NULL,
+        'action',
+        '2021',
+        'Bac Nord',
+        'Olivier Marchal',
+        'Jean Reno, Roschdy Zem, Olivier Marchal',
+        '2012. Les quartiers Nord de Marseille détiennent un triste record : la zone au taux de criminalité le plus élevé de France. Poussée par sa hiérarchie, la BAC Nord, brigade de terrain, cherche sans cesse à améliorer ses résultats. Dans un secteur à haut risque, les flics adaptent leurs méthodes, franchissant parfois la ligne jaune. Jusqu’au jour où le système judiciaire se retourne contre eux…',
+        'https://www.themoviedb.org/t/p/original/r2NnRp4mi4G3e0x9zINQIcnGNd8.jpg',
+        '12',
+        'iyVLnChTs8w',
+        'pari'
+    ),
+    (
+        NULL,
+        'action',
+        '2003',
+        'Le Seigneur des Anneaux : Le Retour du Roi',
+        'Peter Jackson',
+        'Elijah Wood, Ian McKellen, Viggo Mortensen',
+        'Frodon et Sam poursuivent leur route vers Mordor pour détruire l’Anneau unique, tandis que Aragorn, Legolas et Gimli, aidés de Merry et Pippin, continuent à se battre pour le roi Théoden. Ensemble, ils doivent affronter l’armée de Sauron, commandée par le Nazgûl, et affronter le terrible Gollum.',
+        'https://image.tmdb.org/t/p/original/4fcgHdNryRuvEpsldjYifFEVX2i.jpg',
+        '43',
+        'nalLU8i4zgs',
+        'magie'
+    ),
+    (
+        NULL,
+        'action',
+        '2015',
+        'Diversion',
+        'Glenn Ficarra, John Requa',
+        'Will Smith, Margot Robbie, Rodrigo Santoro',
+        'Nicky Spurgeon est un escroc ’professionnel’ à la tête d’une équipe de nombreux arnaqueurs et pickpockets. Un soir, il croise Jess Barrett, une arnaqueuse débutante. Nicky décide de l’intégrer pour un gros coup à La Nouvelle-Orléans. Trois ans plus tard, il la retrouve sur sa route à Buenos Aires...',
+        'https://www.themoviedb.org/t/p/original/AfKm745YiKJ14yp4UfA4jowBIPi.jpg',
+        '9',
+        '_NxDVVyxyjU',
+        'dumb'
+    ),
+    (
+        NULL,
+        'humour',
+        '2012',
+        'Ted',
+        'Seth MacFarlane',
+        'Mark Wahlberg, Mila Kunis, JoeyStarr',
+        'À 8 ans, le petit John Bennett fit le voeu que son ours en peluche de Noël s’anime et devienne son meilleur ami pour la vie, et il vit son voeu exaucé. Presque 30 ans plus tard, l’histoire n’a plus vraiment les allures d’un conte de Noël. L’omniprésence de Ted aux côtés de John pèse lourdement sur sa relation amoureuse avec Lori. Bien que patiente, Lori voit en cette amitié exclusive, consistant principalement à boire des bières et fumer de l’herbe devant des programmes télé plus ringards les uns que les autres, un handicap pour John qui le confine à l’enfance, l’empêche de réussir professionnellement et de réellement s’investir dans leur couple. Déchiré entre son amour pour Lori et sa loyauté envers Ted, John lutte pour devenir enfin un homme, un vrai !',
+        'https://www.themoviedb.org/t/p/original/zdZKj5nqe3tEwuzEYbxAgMESFe7.jpg',
+        '35',
+        '3NPVAUxspdc',
+        'lapinx'
+    ),
+    (
+        NULL,
+        'humour',
+        '2015',
+        'Ted 2',
+        'Seth MacFarlane',
+        'Mark Wahlberg, Mila Kunis, JoeyStarr',
+        'Nouveaux mariés, Ted et Tami-Lynn essayent d’avoir un bébé et demandent à John d’être le donneur en vue d’une insémination artificielle. Cependant, s’il veut avoir la garde de l’enfant, Ted va devoir prouver devant un tribunal qu’il est véritablement humain.',
+        'https://www.themoviedb.org/t/p/original/kZ9cHlWbhtPx5IoR9R1n8UHozej.jpg',
+        '45',
+        'Ewp9umK4V0E',
+        'lapin'
+    )
